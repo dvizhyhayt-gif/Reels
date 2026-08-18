90113c7d9dd19b833fcf346ef9974600e73ff196
dvizhyhayt-gif <dvizhyhayt@gmail.com>
2026-08-18 23:50:01 +0500

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-﻿window.APP_FIREBASE_CONFIG = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
-};
