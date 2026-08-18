d950833dd7f02a899943823e6afab1fb9f305e1b
dvizhyhayt-gif <dvizhyhayt@gmail.com>
2026-08-18 23:49:52 +0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase Configuration
-window.APP_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCIn6UKRIPdaKFuCBZUUW0GYR4fG4eQ9gQ",
-  authDomain: "kazreels.firebaseapp.com",
-  projectId: "kazreels",
-  storageBucket: "kazreels.firebasestorage.app",
-  messagingSenderId: "849520714213",
-  appId: "1:849520714213:web:54975013c201e75a110f0c"
-};
