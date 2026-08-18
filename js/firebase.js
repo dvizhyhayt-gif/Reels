d678cb3c4e5d1a2c4ef1b29298717b7e81f748e9
dvizhyhayt-gif <dvizhyhayt@gmail.com>
2026-08-18 20:47:59 +0500

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -73,7 +73,7 @@ async function approveGame(game){
   await updateDoc(doc(db,'gameSubmissions',game.id),{status:'published',publicUrl:deployed.url,reviewedAt:serverTimestamp()});return deployed.url;
 }
 async function getPublishedGames(){return (await getDocs(collection(db,'publishedGames'))).docs.map(item=>({id:item.id,...item.data()})).filter(x=>x.mediaVersion===2)}
-async function getReviews(){return (await getDocs(collection(db,'gameReviews'))).docs.map(item=>item.data())}
+async function getReviews(){try{return (await getDocs(collection(db,'gameReviews'))).docs.map(item=>item.data())}catch{return []}}
 async function saveReview(gameId,rating,comment){
   if(!auth.currentUser||auth.currentUser.isAnonymous)throw new Error('Войдите в аккаунт');
   const user=await profile(auth.currentUser.uid),slug='published-'+gameId;
